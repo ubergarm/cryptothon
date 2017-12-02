3231b265be82516a4637dd8a51612eba7583de68
Kevin Scott <kevin@hitheory.com>
2017-12-02 13:41:04 -0500

    Try to update the migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var Money = artifacts.require("./Money.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Money);
 };
