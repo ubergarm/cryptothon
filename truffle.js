bb6e9bb4744e45f0777c00fdc1b2710800646154
Sean Matthews <sean.d.matthews@gmail.com>
2017-12-02 16:41:40 -0500

    Adds Rinkeby to truffle config

--- truffle.js
+++ truffle.js
@@ -13,6 +13,13 @@ module.exports = {
             host: "localhost",
             port: 8545,
             gas: 2900000
+        },
+        rinkeby: {
+            host: "localhost", // Connect to geth on the specified
+            port: 8545,
+            from: "0x0085f8e72391Ce4BB5ce47541C846d059399fA6c", // default address to use for any transaction Truffle makes during migrations
+            network_id: 4,
+            gas: 4612388 // Gas limit used for deploys
         }
     },
     rpc: {
